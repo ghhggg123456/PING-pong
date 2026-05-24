e051d248966abd0cba7607add40ab1db8a8f18f0
ghhggg123456 <olshu10@mail.ru>
2026-05-24 21:23:04 +0300

    unfinished

--- script.js
+++
@@ -1,7 +0,0 @@
-const money = document.getElementById('money');
-function moneyUpdate() {
-    value = localStorage.getItem('money');
-    if (parseInt(value)<=0 || typeof value != 'string') { value = 0};
-    money.textContent = value;
-}
-setInterval(moneyUpdate, 100)
